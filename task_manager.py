fc2a72ddb31c9470598a3bd68238a810ba23ac6b
Muhammad Abdullah <muhammad0abdullah68@gamil.com>
2025-05-13 23:22:56 +0500

    get_task_id return false id i've that bug

--- task_manager.py
+++ task_manager.py
@@ -3,11 +3,12 @@ from ui import UIManager
 
 class TaskManager:
     def __init__(self):
-        self.tasks = []
+        self.tasks = StorageManager().load_task()  # Load tasks from the file into the instance variable
+
     # return the id of last task in the list and if no task found it will return 0
     def get_task_id(self):
-        task_list = StorageManager().load_task()
-        last_id =   task_list[-1].get('id', 0) if task_list else 0
+        task_list = self.tasks
+        last_id =   task_list[-1].get('id', 0) if task_list else 0        
         return last_id 
         
     def add_task(self, task):
@@ -25,9 +26,9 @@ class TaskManager:
                 task['description'] = description 
                 StorageManager().save_task(self.tasks)
                 return True
-            else:
-                print(f"No task with id {task_id}")
-                return False
+        else:
+            print(f"No task with id {task_id}")
+            return False
     
     def delete_task(self, task_id):
         for task in self.tasks:
@@ -35,8 +36,7 @@ class TaskManager:
                 self.tasks.remove(task)
                 StorageManager().save_task(self.tasks)
                 return True
-            else:
-                return False
+        return False  # Return False only after checking all tasks
     
     def mark_task_as_completed(self, task_id):
         for task in self.tasks:
@@ -44,5 +44,4 @@ class TaskManager:
                 task['completed'] = True
                 StorageManager().save_task(self.tasks)
                 return True
-            else:
-                return False
+        return False  # Return False only after checking all tasks
