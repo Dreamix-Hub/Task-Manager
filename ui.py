ed93781f17980837e1a2832d6edc67629470884d
Muhammad Abdullah <muhammad0abdullah68@gamil.com>
2025-05-13 23:23:42 +0500

    add message for empty task list in show_tasks method

--- ui.py
+++ ui.py
@@ -14,6 +14,8 @@ class UIManager:
         return input(prompt)
     
     def show_tasks(self, tasks):
+        if not tasks:
+            print("No task found!")
         for task in tasks:
             status = "✅" if task['completed'] else "❌"
             print(
