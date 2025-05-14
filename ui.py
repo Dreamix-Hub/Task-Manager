8c6e327f296ff9f8816ecf4f1d7a489fe28f85b2
Muhammad Abdullah <muhammad0abdullah68@gamil.com>
2025-05-14 23:04:19 +0500

    refactor UIManager methods to use static methods, improving code structure and readability; updated display_menu and show_tasks for better task presentation

--- ui.py
+++ ui.py
@@ -1,26 +1,39 @@
 class UIManager:
     
-    def display_menu(self):
+    @staticmethod
+    def display_menu():
         print("\n============ Task Manager ============")
         print("1. Add Task")    
         print("2. View Tasks")
         print("3. Update Task")
         print("4. Delete Task")
         print("5. Mark Task as Completed")
-        print("6. Save and Exit")
+        print("6. Mark task as incomplete")
+        print("7. show all completed tasks")
+        print("8. show all incomplete tasks")
+        print("9. Save and Exit")
         print("======================================")
         
-    def get_user_input(self, prompt):
+    @staticmethod    
+    def get_user_input(prompt):
         return input(prompt)
     
-    def show_tasks(self, tasks):
+    @staticmethod
+    def show_tasks(tasks):
         if not tasks:
             print("No task found!")
         for task in tasks:
             status = "✅" if task['completed'] else "❌"
             print(
-                f"{task['id']} ~ {task['title'] }: {task['description']} => {status}"
+                f"------------- Task ID: {task['id']} ---------------\n"
+                f"Title: {task['title']}\n"
+                f"Description: {task['description']}\n"
+                f"Status: {status}\n"
+                f"Creation Date: {task['creationDate']}\n"
+                f"Due Date: {task['dueDate']}\n"
+                "----------------------------------------\n"
             )
-    
-    def show_message(self, msg):
+
+    @staticmethod
+    def show_message(msg):
         print(msg)
